ebd752f242f1e139005d54fcde8548d1496964d5
Nash Vail <nishantve1@gmail.com>
2020-08-07 14:04:25 +0530

    Added GT Eesti

--- src/components/Home/LeftSideContent.js
+++ src/components/Home/LeftSideContent.js
@@ -1,9 +1,19 @@
 import React from "react"
 
-const ChapterClicker = ({ number }) => (
+const ChapterClicker = ({ number, name, link }) => (
   <li>
-    <a>
-      Chapter {number}
+    <a href="#">
+      <span style={{
+        fontFamily: 'GT Eesti Pro Text-Regular',
+        fontSize: '20px',
+        display: 'block',
+        color: '#75AAB3'
+      }}>Chapter {number}</span>
+      <span style={{
+        fontFamily: 'GT America-Expanded Medium',
+        fontSize: '30px',
+        color: '#112d32'
+      }}>{name}</span>
     </a>
   </li>
 );
@@ -11,11 +21,12 @@ const ChapterClicker = ({ number }) => (
 const LeftSideContent = () => (
   <section className="home-page__left">
     <h1 style={{ fontSize: '48px '}}>LOVING RECURSION</h1>
-    <h2>Written in English and JavaScript</h2>
+    <h2>Chapters</h2>
     <ol className="home-page__left__chapter-list">
-      <ChapterClicker number={1}/>
-      <ChapterClicker number={2}/>
-      <ChapterClicker number={3}/>
+      <ChapterClicker number={0} name="Introduction" />
+      <ChapterClicker number={1} name="The Encounter" />
+      <ChapterClicker number={2} name="Then Comes Trust" />
+      <ChapterClicker number={3} name="Procedures and Patterns" />
     </ol>
   </section>
 )
