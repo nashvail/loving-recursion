ca928582fced412b32029c57817b0b07096474c3
Nash Vail <nishantve1@gmail.com>
2020-08-18 17:25:25 +0530

    very bad motion things doing I

--- src/components/Home/RightSideContent.js
+++ src/components/Home/RightSideContent.js
@@ -1,16 +1,16 @@
 import React from "react"
+import { motion } from "framer-motion"
 
 import heroineImage from "../../assets/images/heroine.png"
 
 const RightSideContent = () => (
   <section className="home-page__right">
     <section className="home-page__right__hero">
-      <img src={heroineImage} />
+      <motion.img src={heroineImage} initial={{ y: 100 }} animate={{ y: 0 }} transition={{ ease : [0, 0.71, 0.15, 1.03] }} />
     </section>
     <section className="home-page__right__rest">
       <h1>What the fuck</h1>
     </section>
-
   </section>
 )
 
