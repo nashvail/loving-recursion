09ee01f870294f5e4a2987eac5cb76ed222220bb
Nash Vail <nishantve1@gmail.com>
2020-08-26 18:30:43 +0530

    got lcomotive to work

--- src/components/Home/RightSideContent.js
+++ src/components/Home/RightSideContent.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useContext, useState } from "react"
+import React, { useEffect, useContext, useLayoutEffect } from "react"
 import { motion } from "framer-motion"
 import Lottie from "react-lottie"
+import LocomotiveScroll from "locomotive-scroll"
 
 // Local imports
 import heroineImage from "../../assets/images/head.png"
@@ -40,13 +41,21 @@ const CircleExpandLottieFront = ({
 
 const RightSideContent = () => {
   const [scrollContext, setScrollContext] = useContext(RightSideScrollContext)
+  const scrollRef = React.createRef()
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollRef.current,
+      smooth: true,
+    })
+  }, [])
 
   const onSectionScroll = e => {
     setScrollContext(e.target.scrollTop)
   }
 
   return (
-    <section className="home-page__right" onScroll={onSectionScroll}>
+    <section className="home-page__right" ref={scrollRef}>
       <section className="home-page__right__hero">
         <CircleExpandLottieFront
           width={380}
@@ -54,7 +63,12 @@ const RightSideContent = () => {
           className="home-page__right__hero__lottie-1"
           animationData={circleFrontData}
         />
-        <img src={heroineImage} />
+        <img
+          src={heroineImage}
+          data-scroll
+          data-scroll-speed="3"
+          data-scroll-position="top"
+        />
         <CircleExpandLottieFront
           width={200}
           height={200}
@@ -62,7 +76,53 @@ const RightSideContent = () => {
           animationData={circleFrontData2}
         />
       </section>
-      <section className="home-page__right__rest">
+      <section
+        className="home-page__right__rest"
+      >
+        <h2 data-scroll data-scroll-speed="1" data-scroll-position="top">Why is this Book?</h2>
+        <p>
+          This book is because one day an article about recursion that I was
+          writing got too long and too deep to be called just an article. See
+          this project as a collection of articles on the same topic or, a
+          “book”, if you want me to feel better and more pompous than I already
+          do; now that I have finished a project I thought I never would. This
+          book is also a test of my ideas on teaching philosophy that I have
+          come up with after some amount of pondering, about 10% more than a
+          normal human would. More details about this in the Introduction
+          chapter.
+        </p>
+      </section>
+    </section>
+  )
+
+  /*
+  return (
+    <section
+      className="home-page__right"
+      onScroll={onSectionScroll}
+      data-scroll-container
+      ref={scrollRef}
+    >
+      <section className="home-page__right__hero" data-scroll-section>
+        <CircleExpandLottieFront
+          width={380}
+          height={380}
+          className="home-page__right__hero__lottie-1"
+          animationData={circleFrontData}
+        />
+        <img src={heroineImage} data-scroll data-scroll-speed="1" />
+        <CircleExpandLottieFront
+          width={200}
+          height={200}
+          className="home-page__right__hero__lottie-2"
+          animationData={circleFrontData2}
+        />
+      </section>
+      <section
+        className="home-page__right__rest"
+        data-scroll
+        data-scroll-speed="2"
+      >
         <h2>Why is this Book?</h2>
         <p>
           This book is because one day an article about recursion that I was
@@ -78,6 +138,7 @@ const RightSideContent = () => {
       </section>
     </section>
   )
+  */
 }
 
 export default RightSideContent
