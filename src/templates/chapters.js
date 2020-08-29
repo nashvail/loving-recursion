3165393970d717a33b96a12541f1acb70e0fbaef
Nash Vail <nishantve1@gmail.com>
2020-08-29 19:56:13 +0530

    Progressive image dynamic

--- src/templates/chapters.js
+++ src/templates/chapters.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useLayoutEffect } from "react"
 import { motion, AnimatePresence } from "framer-motion"
-import { graphql } from 'gatsby'
+import { graphql } from "gatsby"
+import ProgressiveImage from "react-progressive-image"
 
-// Images
-import heroImage from "../assets/images/hero-chapter-12.png"
+import testSrc from "../assets/images/chapter-heroes/0.png"
 
 let Splitting
 if (typeof window !== `undefined`) {
@@ -11,43 +11,51 @@ if (typeof window !== `undefined`) {
 }
 
 export default ({ children, pageContext }) => {
-
-  const {number, name} = pageContext.frontmatter
+  const chapterNumber = pageContext.frontmatter.number,
+    chapterName = pageContext.frontmatter.name;
 
   useLayoutEffect(() => {
-    Splitting({ by: 'lines'})
+    Splitting({ by: "lines" })
   })
 
   useEffect(() => {
     if (typeof window === "undefined") return null
-    const title = document.querySelector('.chapter-view__title')
+    const title = document.querySelector(".chapter-view__title")
 
     setTimeout(() => {
-      title.classList.add('show') 
-    }, 175);
+      title.classList.add("show")
+    }, 175)
 
     return () => {
-      title.classList.remove('show')
+      title.classList.remove("show")
     }
   }, [])
-
-  console.log(name, number);
 
   return (
     <main className="chapter-view">
       <section className="chapter-view__hero">
-        <h3 className="chapter-view__number">Chapter {number}</h3>
+        <h3 className="chapter-view__number">Chapter {chapterNumber}</h3>
         <h1 className="chapter-view__title" data-splitting>
-          {name}
+          {chapterName}
         </h1>
-        <motion.img
-          src={heroImage}
-          transition={{ ease: [0, 0.71, 0.15, 1.03] }}
-          initial={{ y: 200 }}
-          animate={{ y: 0 }}
-        />
+        <ProgressiveImage src={require(`../assets/images/chapter-heroes/${chapterNumber}.png`)}>
+          {src => (
+             <motion.img
+             src={src}
+             transition={{ ease: [0, 0.71, 0.15, 1.03] }}
+             initial={{ y: 200 }}
+             animate={{ y: 0 }}
+             alt={`Chapter ${chapterNumber} hero image`}
+           />
+          )}
+        </ProgressiveImage>
+       
       </section>
-      <article className="chapter-view__content">{children}</article>
+      <section className="chapter-view__content">
+        <article>
+            {children}
+        </article>
+      </section>
     </main>
   )
 }
