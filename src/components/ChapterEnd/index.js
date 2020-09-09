559f68836ce8b48c66f8ccb19deb95c1fdf459bf
Nash Vail <nishantve1@gmail.com>
2020-09-09 12:08:14 +0530

    Various fixes to the footer

--- src/components/ChapterEnd/index.js
+++ src/components/ChapterEnd/index.js
@@ -2,30 +2,60 @@ import React from "react"
 import PropTypes from "prop-types"
 import { motion } from "framer-motion"
 import ProgressiveImage from "react-progressive-image"
+import { Link } from "gatsby"
 
 // Image imports
 import footer_art from "../../assets/images/footer_art.png"
 
+// Chapter Data
+import chapters from "../../data/chapters.json"
+
 const ChapterEnd = ({ nextChapterNumber }) => {
   return (
     <section className="chapter-end">
-      <div className="chapter-end__next-chapter">
+      <Link
+        to={chapters[nextChapterNumber]["link"]}
+        className="chapter-end__next-chapter"
+      >
         <h4>Next Chapter</h4>
-        <h3>Chapter Name</h3>
+        <h3>{chapters[nextChapterNumber]["chapterName"]}</h3>
         <ProgressiveImage
           src={require(`../../assets/images/chapter-heroes/${nextChapterNumber}.png`)}
         >
           {src => (
-            <motion.img src={src} alt={`Chapter ${nextChapterNumber} hero image`} />
+            <motion.img
+              src={src}
+              alt={`Chapter ${nextChapterNumber} hero image`}
+            />
           )}
         </ProgressiveImage>
-      </div>
+      </Link>
       <footer className="footer">
         <div className="footer__left">
+          <div>
             <h3>By Nash Vail</h3>
+            <ul className="footer__left__links">
+              <li>
+                <a>
+                  <h4>Tweet a Feedback</h4>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <h4>Support</h4>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <h4>Contribute</h4>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <h4>License: CC BY-NC-SA</h4>
         </div>
         <div className="footer__right">
-          <img src={footer_art}/>
+          <img src={footer_art} />
         </div>
       </footer>
     </section>
