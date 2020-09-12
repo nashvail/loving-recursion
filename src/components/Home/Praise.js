4cef83db8f6a1e9df6425f5758dda1d9dfc82440
Nash Vail <nishantve1@gmail.com>
2020-09-12 23:24:02 +0530

    Added dynamic praise

--- src/components/Home/Praise.js
+++ src/components/Home/Praise.js
@@ -1,27 +1,49 @@
 import React from "react"
 
+// Put all the praise and related links in this object
+const praises = [
+  {
+    text: "I wish Nash taught me Math when I was a kid.",
+  },
+  {
+    text:
+      "Came across this today and I don't remember a blog post I had more fun reading.",
+  },
+]
 
-const SinglePraise = ({ direction }) => (
-  <li className="home-page__right__rest__praises__praise">
-    <div className={`home-page__right__rest__praises__praise--text-${direction}`}>
-      <h2>"I wish Nash Taught me math when I was a kid"</h2>
-      <span> - John Doe (Tweet)</span>
-    </div>
-    <div className={`home-page__right__rest__praises__praise--for-${direction}`}>
-      <div className={`home-page__right__rest__praises__praise--for-${direction}__content`}>
-        This is where everything will go
+const SinglePraise = ({ direction, praiseObject }) => {
+  console.log(direction, praiseObject);
+  return (
+    <li className="home-page__right__rest__praises__praise">
+      <div
+        className={`home-page__right__rest__praises__praise--text-${direction}`}
+      >
+        <h2>{`"${praiseObject["text"]}"`}</h2>
+        <span> - John Doe (Tweet)</span>
       </div>
-    </div>
-  </li>
-)
+      <div
+        className={`home-page__right__rest__praises__praise--for-${direction}`}
+      >
+        <div
+          className={`home-page__right__rest__praises__praise--for-${direction}__content`}
+        >
+          This is where everything will go
+        </div>
+      </div>
+    </li>
+  )
+}
 
 export default () => {
   return (
     <ul className="home-page__right__rest__praises">
-      <SinglePraise direction={0}/>
-      <SinglePraise direction={1}/>
-      <SinglePraise direction={0}/>
-      <SinglePraise direction={1}/>
+      {praises.map((praise, index) => (
+        <SinglePraise
+          key={index}
+          direction={index % 2}
+          praiseObject={praises[index]}
+        />
+      ))}
     </ul>
   )
 }
