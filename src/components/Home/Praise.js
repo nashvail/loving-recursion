dcae94b3104776fb767e924ac9c146e4ab86049c
Nash Vail <nishantve1@gmail.com>
2020-09-14 11:54:19 +0530

    Updated praise with Links and authors

--- src/components/Home/Praise.js
+++ src/components/Home/Praise.js
@@ -4,22 +4,42 @@ import React from "react"
 const praises = [
   {
     text: "I wish Nash taught me Math when I was a kid.",
+    workTitle: "How you can use simple Trigonometry to create better loaders",
+    workLink: "https://uxdesign.cc/how-you-can-use-simple-trigonometry-to-create-better-loaders-32a573577eb4",
+    author: "Andrew Harvard",
   },
   {
     text:
-      "Came across this today and I don't remember a blog post I had more fun reading.",
+      "Read a maths article, implement it in code within 30mins (didn’t look at your JS code), that rarely happens. A testament to the quality of this article.",
+    workTitle: "Nerding out with Bezier Curves",
+    workLink: "https://medium.com/free-code-camp/nerding-out-with-bezier-curves-6e3c0bc48e2f",
+    author: "Sahil Singh",
+  },
+  {
+    text:
+      "This is an amazing tutorial. You explain every single detail in manageable pieces. Even for a Maths idiot like myself, I understood it.",
+    workTitle: "How to fix dragging animation in UI with simple Math",
+    workLink: "https://uxdesign.cc/how-to-fix-dragging-animation-in-ui-with-simple-math-4bbc10deccf7",
+    author: "Charles Robertson",
+  },
+  {
+    text:
+      "If Nash taught any other course I'd probably just take it, because I know I would learn something I can put into use right away.",
+    workTitle: "A beginner's introduction to Git and GitHub",
+    workLink: "https://skl.sh/2riYNbD",
+    author: "Hayden B Dennis",
   },
 ]
 
 const SinglePraise = ({ direction, praiseObject }) => {
-  console.log(direction, praiseObject);
+  console.log(direction, praiseObject)
   return (
     <li className="home-page__right__rest__praises__praise">
       <div
         className={`home-page__right__rest__praises__praise--text-${direction}`}
       >
         <h2>{`"${praiseObject["text"]}"`}</h2>
-        <span> - John Doe (Tweet)</span>
+        <span>- {praiseObject.author}</span>
       </div>
       <div
         className={`home-page__right__rest__praises__praise--for-${direction}`}
@@ -27,7 +47,7 @@ const SinglePraise = ({ direction, praiseObject }) => {
         <div
           className={`home-page__right__rest__praises__praise--for-${direction}__content`}
         >
-          This is where everything will go
+          <a href={praiseObject.workLink} target="__blank">{praiseObject.workTitle}</a>
         </div>
       </div>
     </li>
@@ -37,7 +57,7 @@ const SinglePraise = ({ direction, praiseObject }) => {
 export default () => {
   return (
     <ul className="home-page__right__rest__praises">
-      {praises.map((praise, index) => (
+      {praises.map((_, index) => (
         <SinglePraise
           key={index}
           direction={index % 2}
