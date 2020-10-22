61a42fee9f86dc7dc5ced295a37aa7fd838d3abd
Nash Vail <nishantve1@gmail.com>
2020-10-22 20:17:18 +0530

    responsiveness you little shit

--- src/components/MobileSidebar/index.js
+++ src/components/MobileSidebar/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import ChapterClicker from "../../components/Home/ChapterClicker";
 import PropTypes from 'prop-types'
 
 // Import context
@@ -10,7 +11,7 @@ import { ResponsiveIndex } from "../../components/Home/RightSideContent";
 const MobileSidebar = ({ currentChapter = 0 }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useContext(SidebarOpenContext);
 
-  const tY = isSidebarOpen ? '2%' : 'calc(100% - 64px)';
+  const tY = isSidebarOpen ? '0%' : 'calc(100vh - 9rem)';
 
   const toggleMobileSidebar = _ => setIsSidebarOpen(!isSidebarOpen);
 
@@ -23,46 +24,18 @@ const MobileSidebar = ({ currentChapter = 0 }) => {
         <button type="button" onClick={toggleMobileSidebar}>Do Something</button>
       </div>
       <div className="mobile-sidebar__index">
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
-        Hello<br/>
+        <ul>
+          <ChapterClicker number={0} />
+          <ChapterClicker number={1} />
+          <ChapterClicker number={2} />
+          <ChapterClicker number={3} />
+          <ChapterClicker number={4} />
+          <ChapterClicker number={5} />
+          <ChapterClicker number={6} />
+          <ChapterClicker number={7} />
+          <ChapterClicker number={8} />
+          <ChapterClicker number={9} />
+        </ul>
       </div>
     </aside>
   );
